dc9e262535775e6d7a333000c12b52be9a965a22
Muhammadali <odiljonovmuhammadali742@gmail.com>
2026-01-08 20:33:07 +0500

    first

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://oliy-kimyo-erp-frontend-7s97.vercel.app/',
+    origin: 'http://138.249.7.187:3000',
     credentials: true,
   });
 
